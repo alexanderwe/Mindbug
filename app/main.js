f371948a9d48def4805d0bb0de463f0eb904dd42
alexanderwe <weiss_alex@gmx.net>
2016-11-18 11:07:48 +0100

    Added mainmenu.js. Moved dist/* to .gitignore

--- app/main.js
+++ app/main.js
@@ -1,4 +1,5 @@
 const electron = require('electron')
+const {Menu} = require('electron')
 // Module to control application life.
 const app = electron.app
 // Module to create native browser window.
@@ -14,14 +15,14 @@ let mainWindow
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 500,
-    minHeight: 300,
-    minWidth: 520,
-    titleBarStyle: 'hidden',
-    title: 'Mindbug',
-    vibrancy: 'dark'
-});
+      width: 800,
+      height: 500,
+      minHeight: 300,
+      minWidth: 520,
+      titleBarStyle: 'hidden',
+      title: 'Mindbug',
+      vibrancy: 'dark'
+    });
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
@@ -38,8 +39,10 @@ function createWindow () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
+    mainWindow = null;
   })
+
+  require('./mainmenu')
 }
 
 // This method will be called when Electron has finished
---
+++ app/mainmenu.js
@@ -0,0 +1,89 @@
+const {Menu} = require('electron')
+const electron = require('electron')
+const app = electron.app
+
+const template = [
+  {
+    label: 'View',
+    submenu: [
+      {
+        label: 'Toggle Developer Tools',
+        accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
+        click (item, focusedWindow) {
+          if (focusedWindow) focusedWindow.webContents.toggleDevTools()
+        }
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'resetzoom'
+      },
+      {
+        role: 'zoomin'
+      },
+      {
+        role: 'zoomout'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'togglefullscreen'
+      }
+    ]
+  },
+  {
+    role: 'window',
+    submenu: [
+      {
+        role: 'minimize'
+      },
+      {
+        role: 'close'
+      }
+    ]
+  },
+  {
+    role: 'help',
+    submenu: [
+      {
+        label: 'Learn More',
+        click () { require('electron').shell.openExternal('http://electron.atom.io') }
+      }
+    ]
+  }
+]
+
+if (process.platform === 'darwin') {
+  const name = app.getName()
+  template.unshift({
+    label: name,
+    submenu: [
+      {
+        role: 'about'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'hide'
+      },
+      {
+        role: 'hideothers'
+      },
+      {
+        role: 'unhide'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'quit'
+      }
+    ]
+  })
+}
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
